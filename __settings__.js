b88d343f945b710d6835fe67aefd1e2b50ba98ab
ngthanhdat521 <84183823+ngthanhdat521@users.noreply.github.com>
2025-01-15 22:46:05 +0700

    initial game

---
+++ __settings__.js
@@ -0,0 +1,23 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2150860.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': false,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 210474710, 210474705, 210474701, 210474694, 210474749, 210474691, 210474755, 210474706, 210474746, 210474713, 210474709, 210474740 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/210474692/1/basis.wasm.js', 'wasmUrl' : 'files/assets/210474693/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/210474697/1/basis.js', 'preload' : false},
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/210474699/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/210474700/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/210474698/1/ammo.js', 'preload' : true},
+];
